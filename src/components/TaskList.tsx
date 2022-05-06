cf6931f914e96ae5bc030209be9483cf4573dbc9
Biaginibe <49575566+Biaginibe@users.noreply.github.com>
2022-05-06 13:04:41 -0300

    chalenge 01 complete - task manager

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,51 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== '' && newTaskTitle !== null){
+      // Gerando o ID aleatório
+      let id = Math.floor(Math.random() * (50 - 0 + 1)) + 0;
+      
+      // Incluindo a nova tarefa na lista
+      let listOfTasks = tasks
+      listOfTasks.push({
+        id: id,
+        title: newTaskTitle,
+        isComplete: false
+      })
+      setTasks(listOfTasks)
+      console.log(tasks)
+      
+      // Limpando o campo onde foi informado o título da tarefa adicionada
+      setNewTaskTitle('')
+    }else{
+      window.alert('Informe o título da tarefa para adiciona-la.')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    // Função que faz o update
+    let result = tasks.map((elem) => {
+      if(elem.id === id) {
+        elem.isComplete = !elem.isComplete;
+      }
+      return elem;
+    })
+
+    // Setando o novo array
+    setTasks(result);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    // Função que devolve um array sem o item do ID selecionado para a remoção
+    let filter = tasks.filter((elem) => {
+      return elem.id !== id;
+    })
+    // Setando o estado para nosso novo array
+    setTasks(filter);
   }
 
   return (
